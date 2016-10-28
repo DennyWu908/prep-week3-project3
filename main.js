708852e46906dc5f903a76b2408a1728daa28010
Denny Wu <denny.wu87@gmail.com>
2016-10-27 19:39:48 -0700

    Complete basic track questions

--- main.js
+++ main.js
@@ -17,14 +17,15 @@ ADVANCED TRACK: 14-20
 =======================================================
 */
 
-
+document.getElementById("q0").innerHTML = "JS Page Connected Properly!";
+document.getElementById("q0").classList.add("status-good");
 
 // 1. Create an object called "me" that describes you with your name, city, and job title.
 //    Display the content of the object in the HTML page. 
 
+var me = { name: "denny", city: "san francisco", jobTitle: "student" }
 
-
-
+document.getElementById("q1").innerHTML = me.name + ", " + me.city + ", " + me.jobTitle;
 
 // 2. To your "me" object, add a key of "family" with the value being an array of three of your family members and display in the HTML page.
 // Example of desired output: { name: 'Bob Smith',
@@ -32,19 +33,16 @@ ADVANCED TRACK: 14-20
 //                              jobTitle: 'professional wrestler',
 //                              family: ['Joan', 'Kyle', 'Hank'] }
 
+me.family = ["mom", "dad", "sister"]
 
-
-
+document.getElementById("q2").innerHTML = me.name + ", " + me.city + ", " + me.jobTitle + ", " + me.family;
 
 
 // 3. Using the "me" object, use dot notation and bracket notation to access the value of your city and display the results in the HTML page.
 
+document.getElementById("q3").innerHTML = me.city;
 
-
-
-
-
-
+document.getElementById("q3b").innerHTML = me["city"];
 
 // Use the following Object for questions 4-9:
 var invitees = { adults: [{
@@ -84,34 +82,53 @@ var invitees = { adults: [{
 
 // 4. Access Penny's dinner choice (this will be the second item in his dinnerChoice array)
 
+document.getElementById("q4").innerHTML = invitees.adults[0].dinnerChoice;
+
 // 5. Access Pablo's table number.
+
+document.getElementById("q5").innerHTML = invitees.children[1].table;
 
 // 6. Access Lauren's seating specialConsideration.
 
+document.getElementById("q6").innerHTML = invitees.adults[2].specialConsiderations.seating;
+
 // 7. Access Billy's dessert choice (this will be the third item in his dinnerChoice array)
 
+document.getElementById("q7").innerHTML = invitees.adults[1].dinnerChoice[2];
+
 // 8. Access Ada's allergies.
+
+document.getElementById("q8").innerHTML = invitees.children[2].allergies[0];
 
 // 9. Create a string that reads Lauren's dinner choice in a sentence.
 // Desired result: "Lauren's appetizer is _______. She will be eating ______ for dinner
 // and has selected ________ as her dessert."
 
-
-
+document.getElementById("q9").innerHTML = "Lauren's appetizer is " + invitees.adults[2].dinnerChoice[0] + ". " + "She will be eating " + invitees.adults[2].dinnerChoice[1] + " for dinner and has selected " + invitees.adults[2].dinnerChoice[2] + " as her dessert."
 
 
 
 // 10. Create an empty object called "jsWorkshop" using Constructor Notation.
 
+var jsWorkshop = new Object();
+
+document.getElementById("q10").innerHTML = jsWorkshop;
+
 // 11. To your "jsWorkshop" object, assign a key of "classmates" with the value being an array of five your classmates.
+
+jsWorkshop.classmates = ["kevin", "mohammed", "kurtis", "david", "daniel"]
+
+document.getElementById("q11").innerHTML = jsWorkshop.classmates;
 
 // 12. Get the length of the fourth classmate's name.
 
+document.getElementById("q12").innerHTML = jsWorkshop.classmates[3].length;
+
 // 13. Clear the classmates value.
 
+jsWorkshop.classmates = "this is a blank string"
 
-
-
+document.getElementById("q13").innerHTML = jsWorkshop.classmates;
 
 
 // ADVANCED TRACK
